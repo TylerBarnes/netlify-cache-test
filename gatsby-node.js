f4528ffec6dda6d31f64fa68cbf61bdb121176f8
Tyler Barnes <tylerdbarnes@gmail.com>
2020-03-31 16:56:12 -0700

    add caching example

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,14 @@
  */
 
 // You can delete this file if you're not using it
+exports.createPages = async ({ cache }) => {
+  const cache_key = "test-time"
+  const lastTestTime = await cache.get(cache_key)
+
+  console.log(`last test time ${lastTestTime}`)
+
+  const time = Date.now()
+  console.log(`caching test time ${time}`)
+
+  await cache.set(cache_key, time)
+}
